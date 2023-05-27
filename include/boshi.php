ce2cb303ba9df9547a0f6129dcea304f860b46ec
Jabborov Ilhom <jabborovilhom300@gmail.com>
2023-05-27 10:54:49 +0300

    saytni hider va futer qismi

---
+++ include/boshi.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Makiyaj sayti</title>
+</head>
+<body>
+    <div >
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none ml-5">
+            <img src="./rasmlar/emblema.jpg" alt="emblema" width="60" hight="62">
+            <span class="fs-4">Salon : MAKIYAJ</span>
+        </a>
+
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Bosh sahifa</a></li>
+            <li class="nav-item"><a href="../namuna.php" class="nav-link">Namuna</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Xizmatlar</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Mutaxasislar</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Biz bilan bog'lanish</a></li>
+        </ul>
+        </header>
+    </div>
---
+++ include/oxiri.php
@@ -0,0 +1,20 @@
+<div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                <img src="./rasmlar/emblema.jpg" alt="sayt emplemasi" width="40" hight="34">
+            </a>
+            <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 Company, Inc</span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-body-secondary" href="#"><img src=" ./rasmlar/telegram.png" alt="telegram" width="23" hight="23"></a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="#"><img src="./rasmlar/insta.png" alt="instagram" width="24" hight="24"></a></li>
+            <li class="ms-3"><a class="text-body-secondary" href="#"><img src="./rasmlar/vatsap.png" alt="xullass qanday yozilishi esimda yo'q -> vatsap" width="24" hight="24"></a></li>
+            </ul>
+        </footer>
+    </div>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
